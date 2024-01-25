13cd07287c4343a1a8f2fc0324f1f9c761537123
王良 <841369634@qq.com>
2024-01-25 17:27:32 +0800

    日志中的dns转大写DNS

--- packages/mitmproxy/src/lib/dns/https.js
+++ packages/mitmproxy/src/lib/dns/https.js
@@ -26,7 +26,7 @@ module.exports = class DNSOverHTTPS extends BaseDNS {
       }
       return ret
     } catch (err) {
-      log.warn('dns query error:', hostname, ', dns:', this.dnsServer, ', errorMsg:', err.message, ', error:', JSON.stringify(err))
+      log.warn('DNS query error:', hostname, ', dns:', this.dnsServer, ', errorMsg:', err.message, ', error:', JSON.stringify(err))
       return []
     }
   }
