c3d4c964048dcb5f01001cc64c11fefc4df38d4f
王良 <841369634@qq.com>
2024-03-01 11:03:18 +0800

    个性化配置

--- packages/core/src/config/index.js
+++ packages/core/src/config/index.js
@@ -21,7 +21,7 @@ module.exports = {
     },
     remoteConfig: {
       enabled: true,
-      url: 'https://gitee.com/wangliang181230/dev-sidecar/raw/remote_config/packages/core/src/config/remote_config.json'
+      url: 'https://gitee.com/wangliang181230/dev-sidecar/raw/remote_config/packages/core/src/config/remote_config_myself.json'
     },
     dock: {
       hideWhenWinClose: false
