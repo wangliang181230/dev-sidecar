632c788f390bcfe02e3f6d0291acbbf43bedd795
王良 <841369634@qq.com>
2024-04-02 10:06:58 +0800

    配置优化。

--- packages/core/src/config/index.js
+++ packages/core/src/config/index.js
@@ -68,7 +68,16 @@ module.exports = {
           desc: 'clone加速复制链接脚本'
         },
         '/.*': {
+          desc: '目前禁掉sni就可以直接访问，如果后续github.com的ip被封锁，只能再走proxy模式',
           sni: 'baidu.com'
+        },
+        '/fluidicon.png': {
+          cacheDays: 365,
+          desc: 'Github那只猫的图片，缓存1年'
+        },
+        '^(/[^/]+){2}/pull/\\d+/open_with_menu.*$': {
+          cacheDays: 7,
+          desc: 'PR详情页：标题右边那个Code按钮的HTML代理请求地址，感觉上应该可以缓存。暂时先设置为缓存7天'
         }
       },
       'github-releases.githubusercontent.com': {
@@ -84,6 +93,10 @@ module.exports = {
       'camo.githubusercontent.com': {
         '.*': {
           sni: 'baidu.com'
+        },
+        '^[a-zA-Z0-9/]+(\\?.*)?$': {
+          cacheDays: 365,
+          desc: '图片，缓存1年'
         }
       },
       'collector.github.com': {
@@ -96,19 +109,40 @@ module.exports = {
           sni: 'baidu.com'
         }
       },
-      'user-images.githubusercontent.com': {
+      'raw.githubusercontent.com': {
         '.*': {
           sni: 'baidu.com'
         }
       },
+      'user-images.githubusercontent.com': {
+        '.*': {
+          sni: 'baidu.com'
+        },
+        '^/.*\\.png(\\?.*)?$': {
+          cacheDays: 365,
+          desc: '用户在PR或issue等内容中上传的图片，缓存1年。注：每张图片都有唯一的ID，不会重复，可以安心缓存'
+        }
+      },
+      'private-user-images.githubusercontent.com': {
+        '.*': {
+          sni: 'baidu.com'
+        },
+        '^/.*\\.png(\\?.*)?$': {
+          cacheHours: 1,
+          desc: '用户在PR或issue等内容中上传的图片，缓存1小时就够了，因为每次刷新页面都是不一样的链接。'
+        }
+      },
       'avatars.githubusercontent.com': {
-        '^/u/\\d+.*$': {
+        '.*': {
+          sni: 'baidu.com'
+        },
+        '^/u/\\d+(\\?.*)?$': {
           cacheDays: 365,
           desc: '用户头像，缓存1年'
         }
       },
       'api.github.com': {
-        '/_private/browser/stats': {
+        '^/_private/browser/stats$': {
           success: true,
           desc: 'github的访问速度分析上传，没有必要，直接返回成功'
         }
@@ -147,52 +181,29 @@ module.exports = {
       'ajax.googleapis.com': {
         '.*': {
           proxy: 'ajax.lug.ustc.edu.cn',
-          backup: [
-            'gapis.geekzu.org'
-          ],
+          backup: ['gapis.geekzu.org'],
           test: 'ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'
         }
       },
       'fonts.googleapis.com': {
         '.*': {
           proxy: 'fonts.geekzu.org',
-          backup: [
-            'fonts.loli.net'
-          ],
+          backup: ['fonts.loli.net'],
           test: 'https://fonts.googleapis.com/css?family=Oswald'
         }
       },
       'themes.googleapis.com': {
         '.*': {
           proxy: 'themes.loli.net',
-          backup: [
-            'themes.proxy.ustclug.org'
-          ]
+          backup: ['themes.proxy.ustclug.org']
         }
       },
       'themes.googleusercontent.com': {
-        '.*': {
-          proxy: 'google-themes.proxy.ustclug.org'
-        }
+        '.*': { proxy: 'google-themes.proxy.ustclug.org' }
       },
-      'clients*.google.com': {
-        '.*': {
-          abort: false,
-          desc: '设置abort：true可以快速失败，节省时间'
-        }
-      },
-      'www.googleapis.com': {
-        '.*': {
-          abort: false,
-          desc: '设置abort：true可以快速失败，节省时间'
-        }
-      },
-      'lh*.googleusercontent.com': {
-        '.*': {
-          abort: false,
-          desc: '设置abort：true可以快速失败，节省时间'
-        }
-      },
+      'clients*.google.com': { '.*': { abort: false, desc: '设置abort：true可以快速失败，节省时间' } },
+      'www.googleapis.com': { '.*': { abort: false, desc: '设置abort：true可以快速失败，节省时间' } },
+      'lh*.googleusercontent.com': { '.*': { abort: false, desc: '设置abort：true可以快速失败，节省时间' } },
       '*.s3.1amazonaws1.com': {
         '/sqlite3/.*': {
           redirect: 'npm.taobao.org/mirrors'
