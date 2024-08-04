144f159c5d5108fadbff84842cb877e3ace5fa1d
王良 <841369634@qq.com>
2024-08-05 02:56:11 +0800

    提交 抢锚位（基于 `dev-sidecar` + `百度OCR`）.js

--- _script/抢锚位（基于 `dev-sidecar` + `百度OCR`）.js
+++ _script/抢锚位（基于 `dev-sidecar` + `百度OCR`）.js
@@ -13,6 +13,9 @@
 (function() {
     'use strict';
 
+    let defaultInterval = 100; // 默认验证码图片识别执行间隔（按需手动调整此配置值）
+    let interval = defaultInterval; // 当前验证码图片识别执行间隔
+
     let img;
     let count = 0;
     let startTime = null;
@@ -39,7 +42,7 @@
         canvas.height = img.height;
         var ctx = canvas.getContext('2d');
         ctx.drawImage(img, 0, 0, img.width, img.height);
-        return canvas.toDataURL('image/png').replace("data:image/png;base64,", "");
+        return canvas.toDataURL('image/png').replace('data:image/png;base64,', '');
     }
     // 刷新图片验证码
     function flushImg () {
@@ -66,16 +69,19 @@
         if (!img.onload) {
             img.onload = function() {
                 if (!img) return;
-                console.info('--------------------------------------------------------------');
-                console.debug('刷新图片验证码');
-                doParse(img);
+                setTimeout(function () {
+                    console.info('--------------------------------------------------------------');
+                    debug('刷新图片验证码');
+                    doParse(img);
+                }, interval);
+                interval = defaultInterval;
             };
         }
         if (!img.onerror) {
             img.onerror = function() {
                 if (!img) return;
                 console.info('--------------------------------------------------------------');
-                console.warn('验证码图片加载失败了，重新加载验证码');
+                warn('验证码图片加载失败了，重新加载验证码');
                 flushImg(img);
             };
         }
@@ -96,36 +102,36 @@
 
         try {
             let imageBase64 = parseImageBase64(img);
-            //console.info("imageBase64.length:", imageBase64.length);
+            //info('imageBase64.length:', imageBase64.length);
             if (!imageBase64 || imageBase64.length < minBase64Length) return;
-            if (img.base64) return console.warn("正在识别图片，不重复发起识别");
+            if (img.base64) return debug('正在识别图片，不重复发起识别');
             imageBase64 = encodeURIComponent(imageBase64);
             img.base64 = imageBase64;
             count++;
             fetchParse(imageBase64);
         } catch(e) {
-            console.warn('验证码识别出现异常：', e);
+            warn('验证码识别出现异常：', e);
             return flushImg();
         }
     }
 
     function fetchParse(imageBase64) {
         fetch('/baiduOcr?' + imageBase64, { method: 'POST' }).then(response => {
-            console.debug('<-- 调用图片识别接口完成：', response.status);
+            debug('<-- 调用图片识别接口完成：', response.status);
 
-            if (img && img.base64 !== imageBase64) return console.warn("图片已变更，丢弃此次识别结果");
+            if (img && img.base64 !== imageBase64) return warn('图片已变更，丢弃此次识别结果');
             if (!enable || !img) {
                 img.base64 = null;
                 return;
             }
 
             if (!response.ok) {
-                console.warn('图片识别接口报错：', response);
+                warn('图片识别接口报错：', response);
                 return flushImg();
             }
             response.json().then(data => {
                 if (data.error_code) {
-                    console.warn(`调用图片识别接口，但是报错了：code: ${data.error_code}, msg: ${data.error_msg}`);
+                    warn(`调用了图片识别接口，但接口报错了：code: ${data.error_code}, msg: ${data.error_msg}`);
                     if (data.error_code === 17) {
                         return fetchParse(imageBase64); // 所使用的百度云账号已达到每日请求限制
                     } else {
@@ -134,30 +140,33 @@
                 }
 
                 if (!data.words_result || data.words_result.length === 0) {
-                    //console.debug('图片识别结果中，没有数字：', decodeURIComponent(imageBase64));
-                    console.debug('图片识别结果中，没有数字：base64长度 =', decodeURIComponent(imageBase64).length);
+                    //debug('图片识别结果中，没有数字：', decodeURIComponent(imageBase64));
+                    debug('图片识别结果中，没有数字：图片base64长度 =', decodeURIComponent(imageBase64).length);
                     return flushImg();
                 }
 
                 // data 格式如：{"words_result":[{"words":"652"}],"words_result_num":1,"log_id":1818914384408222200}
                 const originText = data.words_result[0].words;
-                const text = originText.replaceAll(/[^\d]/g, '');
+                if (!originText) {
+                    debug('识别结果为空：', originText);
+                    return flushImg();
+                }
+                const text = originText.replaceAll(/\D/g, '');
                 if (!text) {
-                    console.debug('识别结果为空：', originText);
+                    debug('识别结果不是数字：', originText);
                     return flushImg();
                 }
-
                 if (!text.match('^\\d{4}$')) {
-                    console.debug('识别结果不是4位数字：', text);
+                    debug('识别结果不是4位数字：', originText);
                     return flushImg();
                 }
 
-                console.info(`识别结果为4位数字：${text}，尝试提交验证`); // 输出验证码识别结果
+                info(`识别结果为4位数字：${text}，尝试提交验证`); // 输出验证码识别结果
 
                 // 自动填入验证码
-                var input = getInput();
+                const input = getInput();
                 if (!input) {
-                    console.warn('未找到验证码输入框，无法自动填入验证码：', text);
+                    warn('未找到验证码输入框，无法自动填入验证码：', text);
                     alert('程序未找到验证码输入框，无法自动填入验证码');
                     img.base64 = null;
                     return;
@@ -166,50 +175,73 @@
                 input.dispatchEvent(new Event('input'));
 
                 // 自动点击按钮
-                var btn = getBtn();
+                let btn = getBtn();
                 if (!btn) {
                     btn = document.querySelector('button.btn-login.ant-btn.ant-btn-primary');
                 }
                 if (!btn) {
-                    console.warn('未找到“确定”按钮，无法提交验证码：', text);
+                    warn('未找到“确定”按钮，无法提交验证码：', text);
                     alert('程序未找到“确定”按钮，无法提交验证码');
                     img.base64 = null;
                     return;
                 }
 
+                interval = 600 + defaultInterval;
                 btn.click();
 
                 img.base64 = null;
                 endTime = Date.now();
 
                 setTimeout(function() {
-                    if (document.querySelector('p.after-title') || location.hash === '#/home') {
-                        checkResult();
-                        return;
-                    }
-
-                    var errorMsgElement = document.querySelector('.ant-message .ant-message-error');
-                    if (errorMsgElement) {
-                        var errorMsg = errorMsgElement.innerText;
-                        if (errorMsg && errorMsg.indexOf('验证码错误') >= 0) {
-                            console.warn('提交了验证码，但验证码错误，识别结果不正确：', text);
-                        } else {
-                            console.warn(`提交了验证码 ${input.value}，但失败了，错误信息：${errorMsg}`);
-                        }
-                    }
+                    checkBtnClick(text);
                 }, 500);
             });
         }).catch(error => {
-            console.warn('验证码识别过程中出错：', error);
+            warn('验证码识别过程中出错：', error);
             return flushImg();
         });
+    }
+
+    function checkBtnClick (text) {
+        if (document.querySelector('p.after-title') || location.hash === '#/home') {
+            checkResult();
+            return;
+        }
+
+        const errorMsgElement = document.querySelector('.ant-message .ant-message-error');
+        if (errorMsgElement) {
+            const errorMsg = errorMsgElement.innerText;
+            if (!errorMsg) return;
+
+            if (errorMsg.indexOf('验证码错误') >= 0) {
+                warn('提交了验证码，但验证码错误，识别结果不正确：', text);
+            } else if (errorMsg.indexOf('该锚位已在') === 0 || errorMsg.indexOf('如要申请请联系') > 0) {
+                warn(`提交了验证码 ${text}，但该锚位已被其他人抢走了，停止识别验证码，有需要时再按F2开启！！！  错误信息：${errorMsg}`);
+
+                // 停止识别验证码
+                stop();
+
+                // 提示已被抢走
+                try {
+                    showMsg(`很遗憾，该锚位已被其他人抢走了，停止识别验证码，有需要时再按F2开启！！！！！！此次尝试识别验证码图片次数：${count} 次，总计耗时：${(endTime - startTime) / 1000} 秒`);
+                } catch(e) {
+                }
+                count = 0;
+                startTime = null;
+                endTime = null;
+            } else {
+                warn(`提交了验证码 ${text}，但失败了，错误信息：${errorMsg}`);
+            }
+        } else {
+            warn(`提交了验证码 ${text}，但未找到执行结果信息，不知道有没有成功或失败！`);
+        }
     }
 
     function start () {
         if (window.qmwInterval || enable) return;
 
         enable = true;
-        console.info('>>> 开始抢锚位！！！');
+        info('>>> 开始抢锚位！！！');
         checkDoParse();
         window.qmwInterval = setInterval(checkDoParse, 100);
     }
@@ -219,11 +251,37 @@
         enable = false;
         clearInterval(window.qmwInterval);
         window.qmwInterval = null;
-        console.info('--- 停止抢锚位！！！');
+        info('--- 停止抢锚位！！！');
     }
 
-    function showSuccess (msg) {
-        console.warn(msg);
+
+    function logTime() {
+        const time = new Date();
+        const hours = String(time.getHours()).padStart(2, '0');
+        const minutes = String(time.getMinutes()).padStart(2, '0');
+        const seconds = String(time.getSeconds()).padStart(2, '0');
+        const milliseconds = String(time.getMilliseconds()).padStart(3, '0');
+
+        return `${hours}:${minutes}:${seconds}.${milliseconds}   `;
+    }
+    function log(args) {
+        let str = logTime();
+        for (let i = 0; i < args.length; i++) {
+            str += ' ' + args[i];
+        }
+        return str;
+    }
+    function debug() {
+        console.debug(log(arguments));
+    }
+    function info() {
+        console.info(log(arguments));
+    }
+    function warn() {
+        console.warn(log(arguments));
+    }
+    function showMsg (msg) {
+        warn(msg);
 
         try {
             if (window.Notification) {
@@ -248,19 +306,19 @@
     function checkResult () {
         if (count === 0 || endTime === null) return false;
 
-        var title;
-        var logFun;
+        let title;
+        let logFun;
 
-        var successElement = document.querySelector('p.after-title');
+        const successElement = document.querySelector('p.after-title');
         if (successElement && successElement.innerText && successElement.innerText.indexOf('成功') > 0) {
             title = '抢锚位成功啦！！！';
-            logFun = showSuccess;
+            logFun = showMsg;
         } else if (location.hash === '#/home') {
             title = '登录成功啦！！！';
-            logFun = showSuccess;
+            logFun = showMsg;
         } else {
             title = `很遗憾，${ location.hash === '#/login' ? '登录' : '抢锚位' }未能成功。`;
-            logFun = console.warn;
+            logFun = warn;
         }
 
         try {
@@ -288,7 +346,7 @@
 
     if (window.Notification) {
         if (Notification.permission !== 'denied' && Notification.permission !== 'granted') {
-            Notification.requestPermission();
+            Notification.requestPermission().then(r => info('“消息通知” 功能授权结果：' + r));
         }
     }
 
