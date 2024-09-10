d97fb85f71b8330431f8b16bca37177d65977b63
王良 <841369634@qq.com>
2024-09-11 05:41:25 +0800

    feature: `Git.exe` 代理，支持排除自定义仓库地址，不进行代理 (#350)
    (cherry picked from commit 34a4a74de59a038ec53608acbb99051b7966edae)

--- packages/core/src/modules/plugin/git/config.js
+++ packages/core/src/modules/plugin/git/config.js
@@ -3,6 +3,10 @@ module.exports = {
   enabled: true,
   tip: '如果你没有安装git命令行则不需要启动它',
   setting: {
-    sslVerify: true // 是否关闭sslVerify
+    sslVerify: true, // Git.exe 是否关闭sslVerify，true=关闭 false=开启
+    noProxyUrls: {
+      'https://gitee.com/': true,
+      'https://e.coding.net/': true
+    }
   }
 }
