3d1b824cedfd8b4b86204903374ede06bd308167
王良 <841369634@qq.com>
2025-02-27 09:17:56 +0800

    再次尝试打包 linux flatpak包

--- packages/gui/vue.config.js
+++ packages/gui/vue.config.js
@@ -104,6 +104,10 @@ module.exports = defineConfig({
               arch: ['x64', 'arm64', 'armv7l'],
             },
             {
+              target: 'flatpak',
+              arch: ['x64', 'arm64', 'armv7l'],
+            },
+            {
               target: 'tar.gz',
               arch: ['x64', 'arm64', 'armv7l'],
             },
