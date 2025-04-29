f8878e8bc8f33d3144b5b42319089bb7ca0aace6
王良 <841369634@qq.com>
2025-04-29 17:21:52 +0800

    revert sni config: [empty]

--- packages/mitmproxy/src/lib/interceptor/impl/req/proxy.js
+++ packages/mitmproxy/src/lib/interceptor/impl/req/proxy.js
@@ -121,8 +121,8 @@ module.exports = {
     if (interceptOpt.sni) {
       let unVerifySsl = rOptions.agent.options.rejectUnauthorized === false
 
-      rOptions.servername = interceptOpt.sni === '[empty]' ? '' : interceptOpt.sni
-      if (interceptOpt.ssl !== true && rOptions.agent.options.rejectUnauthorized && rOptions.agent.unVerifySslAgent) {
+      rOptions.servername = interceptOpt.sni
+      if (rOptions.agent.options.rejectUnauthorized && rOptions.agent.unVerifySslAgent) {
         // rOptions.agent.options.rejectUnauthorized = false // 不能直接在agent上进行修改属性值，因为它采用了单例模式，所有请求共用这个对象的
         rOptions.agent = rOptions.agent.unVerifySslAgent
         unVerifySsl = true
